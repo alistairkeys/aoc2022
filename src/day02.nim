1bcea01e82ed0ef56f05bd970c826f286a3c72c8
alistairkeys <alistair_keys@hotmail.com>
2022-12-02 07:47:32 +0000

    Day 2
    The score is 0, 1, 2 for easier mod so that implies the win, draw, loss
    needs to be one higher (+7, +4, +1) to account for the score being one
    lower.

---
+++ src/day02.nim
@@ -0,0 +1,41 @@
+import std/[strutils, sugar]
+
+# https://adventofcode.com/2022/day/2
+
+func score(letter: string): int =
+  case letter
+    of "A", "X": 0
+    of "B", "Y": 1
+    of "C", "Z": 2
+    else: raise newException(ValueError, "Unexpected input type")
+
+proc rockPaperScissors(filename: string, scoreMe: proc(theirInput,
+    myInput: int): int): int =
+  for l in filename.lines:
+    let
+      parts = l.split(' ')
+      theirInput = score(parts[0])
+      myInput = score(parts[1])
+    inc result, scoreMe(theirInput, myInput)
+
+proc part1(filename: string): int =
+  rockPaperScissors(filename, (theirInput, myInput) => (
+    if myInput == theirInput: myInput + 4
+    elif myInput == ((theirInput + 1) mod 3): myInput + 7
+    else: myInput + 1
+  ))
+
+proc part2(filename: string): int =
+  rockPaperScissors(filename, (theirInput, myInput) => (
+    case myInput
+    of 0: ((theirInput + 2) mod 3) + 1 # lose
+    of 1: theirInput + 4 # draw
+    else: ((theirInput + 1) mod 3) + 7 # win
+  ))
+
+when isMainModule:
+  doAssert part1("../data/day02_example.txt") == 15
+  echo part1("../data/day02_input.txt")
+
+  doAssert part2("../data/day02_example.txt") == 12
+  echo part2("../data/day02_input.txt")
